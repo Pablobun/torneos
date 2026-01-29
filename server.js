8e149a87451d063adb5a22f028349effa58e806a
Pablobun <38007140+Pablobun@users.noreply.github.com>
2026-01-29 04:51:28 +0000

    geminiia

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 
 const app = express();
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 //app.use(cors());
 // Middleware para manejar CORS
 app.use((req, res, next) => {
