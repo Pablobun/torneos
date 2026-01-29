a295efb8081dcba0ba043597741325ef85504203
Pablobun <38007140+Pablobun@users.noreply.github.com>
2026-01-29 05:12:21 +0000

    geminimil

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-001" });
 //app.use(cors());
 // Middleware para manejar CORS
 app.use((req, res, next) => {
